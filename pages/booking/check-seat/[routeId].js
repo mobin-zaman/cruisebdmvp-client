15bb9233006304fe4c0f0371ba8e70c03fdba506
mobin-zaman <mobinzamandev@gmail.com>
2020-10-19 15:34:52 +0600

    available seat dropdown is almost complete, need to rfix a bug in the server

--- pages/booking/check-seat/[routeId].js
+++ pages/booking/check-seat/[routeId].js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import getSeatInfo from '../../../api-service/seat-info';
 import { Dropdown, Image } from "semantic-ui-react";
-import {useState} from 'react';
+import { useState } from 'react';
 
 function CheckSeatPage({ data }) {
   const router = useRouter()
@@ -26,7 +26,7 @@ function CheckSeatPage({ data }) {
   }
 
   const getSeatLayoutImageUrl = (seatCategoryId) => {
-    if(seatCategoryId === null) return null;
+    if (seatCategoryId === null) return null;
 
     const seatCategory = data.find(x => x.seatCategoryId === seatCategoryId);
 
@@ -36,10 +36,20 @@ function CheckSeatPage({ data }) {
   }
 
   const getAvailableSeats = (seatCategoryId) => {
-    if(seatCategoryId === null) return null;
+    if (seatCategoryId === null) return null;
     const seatCategory = data.find(x => x.seatCategoryId === seatCategoryId);
 
-    console.log("available seats: ",seatCategory.availableSeats);
+    console.log("available seats: ", seatCategory.availableSeats);
+
+    const availableSeats  =  seatCategory.availableSeats.map(element => (
+      {
+        key: element.seatId,
+        value: element.seatId,
+        text: element.seatName
+      }
+    ));
+
+    return availableSeats;
 
   }
 
@@ -59,20 +69,32 @@ function CheckSeatPage({ data }) {
           options={seatCategoryOption}
           button
           onChange={getSeatCategory}
-
         />
       </div>
 
       {selectedSeatCategory ? (
         <>
-        {getAvailableSeats(selectedSeatCategory)}
-       <div> Seat Layout</div>
-      <div>
-        <Image src={getSeatLayoutImageUrl(selectedSeatCategory)}/>
-      </div>
-      </>
+          {getAvailableSeats(selectedSeatCategory)}
+          <div> Seat Layout</div>
+          <div>
+            <Image src={getSeatLayoutImageUrl(selectedSeatCategory)} />
+          </div>
+          <div>Available seats for purchase</div>
+          <div>
+            <Dropdown
+              placeholder=' Select seats'
+              search
+              fluid
+              selection
+              multiple
+              options={getAvailableSeats(selectedSeatCategory)}
+              button
+            // onChange={g}
+            />
+          </div>
+        </>
       ) : null
-      } 
+      }
     </>
   );
 }
