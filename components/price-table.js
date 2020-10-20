a9a0ed50c4d772a937392b24a6b245cf7b6279e5
mobin-zaman <mobinzamandev@gmail.com>
2020-10-20 09:53:24 +0600

    pricing table is being updated

--- components/price-table.js
+++ components/price-table.js
@@ -1,16 +1,53 @@
-import {Icon, Label, Menu, Table } from 'semantic-ui-react';
+import { Icon, Label, Menu, Table } from 'semantic-ui-react';
 
-export default function PricingTable({data}) {
+export default function PricingTable({ data }) {
     console.log("Data in the pricing table: ", data);
-    return (
-        <Table celled>
-            <Table.Header>
+    if (!data) return null;
+
+    const createRowAndCell = (data) => {
+        console.log("data: ", data)
+
+        return data.map(element => {
+            const { seatName, seatFare, seatTypeTitle } = element;
+            return (
                 <Table.Row>
-                    <Table.HeaderCell>Ticket</Table.HeaderCell>
-                    <Table.HeaderCell>Price</Table.HeaderCell>
-                    <Table.HeaderCell>Type</Table.HeaderCell>
+
+                    <Table.Cell>
+                        {seatName}
+                    </Table.Cell>
+
+                    <Table.Cell>
+                        {seatFare}
+                    </Table.Cell>
+
+                    <Table.Cell>
+                        {seatTypeTitle}
+                    </Table.Cell>
+
                 </Table.Row>
-            </Table.Header>
-        </Table>
-    )
+            )
+        })
+    }
+
+
+
+
+return (
+    <Table celled>
+
+        <Table.Header>
+            <Table.Row>
+                <Table.HeaderCell>Ticket</Table.HeaderCell>
+                <Table.HeaderCell>Fare</Table.HeaderCell>
+                <Table.HeaderCell>Type</Table.HeaderCell>
+            </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+            {
+                createRowAndCell(data)
+            }
+        </Table.Body>
+    </Table>
+)
 }
