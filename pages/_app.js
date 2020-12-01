edb18147864f500bb40de631608da3e38f65b702
mobin-zaman <mobinzamandev@gmail.com>
2020-12-01 13:53:02 +0600

    firebase clients has been added

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,18 @@ import '../styles/globals.css'
 import Router from 'next/router';
 import NProgress from 'nprogress'; //nprogress module
 import 'nprogress/nprogress.css'; //styles of nprogress
+import {AuthProvider} from '../context/user.context';
 
 Router.events.on('routeChangeStart', () => NProgress.start()); 
 Router.events.on('routeChangeComplete', () => NProgress.done()); 
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+  <Component {...pageProps} />
+  </AuthProvider>
+  )
 }
 
 export default MyApp
