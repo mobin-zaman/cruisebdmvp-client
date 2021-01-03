864dd0f873e2dda66573fea39dc72534c47f37c4
mobin-zaman <mobinzamandev@gmail.com>
2021-01-03 11:25:16 +0600

    base url changed

--- api-service/base-url.js
+++ api-service/base-url.js
@@ -1,3 +1,4 @@
 
 // export const BASE_URL = "http://192.168.0.100:4000";
-export const BASE_URL = "https://ecorivercruise.com";
+// export const BASE_URL = "https://ecorivercruise.com";
+export const BASE_URL = "https://machbikroy.com";
